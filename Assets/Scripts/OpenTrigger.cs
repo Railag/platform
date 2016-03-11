77d7167b95ec8b8c12fde61d44554b110a790e16
Railag <whenthegroundcavedin@gmail.com>
2016-03-11 14:15:13 +0300

    open trigger with rock object, some floor prefabs

---
+++ Assets/Scripts/OpenTrigger.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenTrigger : Trigger {
+	[SerializeField] private GameObject _objectToHide;
+
+	private bool hidden = false;
+
+	private bool locked = false;
+
+	private float distance = 0f;
+
+	void Start() {
+		distance = _objectToHide.GetComponent<Renderer> ().bounds.size.y;
+	}
+
+	override
+	public void PlayerCollide() {
+
+		if (locked)
+			return;
+
+		float y = hidden ? distance : -distance;
+		Vector3 movement = new Vector3 (0, y, 0);
+
+		iTween.MoveBy (_objectToHide, movement, 2f);
+		StartCoroutine (Unlock (2f));
+
+		locked = true;
+
+		hidden = !hidden;
+	}
+
+	IEnumerator Unlock(float seconds) {
+		yield return new WaitForSeconds (seconds);
+
+		locked = false;
+	}
+}
