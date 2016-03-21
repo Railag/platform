5c8894c53a74d21d8c3d97515b2a3e7292e9b294
Railag <whenthegroundcavedin@gmail.com>
2016-03-21 13:04:38 +0300

    ground open trigger, some assets

---
+++ Assets/Scripts/GroundMovingTrigger.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundMovingTrigger : Trigger
+{
+
+	[SerializeField] GameObject ground;
+	[SerializeField] Direction direction;
+	[SerializeField] float seconds = 3.0f;
+	[SerializeField] float distance = 5.0f;
+	[SerializeField] Sprite activeSprite;
+
+	private Sprite inactiveSprite;
+
+	private bool active = false;
+
+	private Vector3 distanceVector;
+
+	enum Direction
+	{
+		left,
+		right
+	}
+
+	void Start ()
+	{
+		distanceVector = new Vector3 (distance, 0f, 0f);
+		inactiveSprite = GetComponent<SpriteRenderer> ().sprite;
+	}
+
+	override
+	public void PlayerCollide ()
+	{
+		if (active)
+			return;
+		
+		active = true;
+		GetComponent<SpriteRenderer> ().sprite = activeSprite;
+
+		switch (direction) {
+		case Direction.left:
+			iTween.MoveBy (ground, -distanceVector, seconds);
+			break;
+		case Direction.right:
+			iTween.MoveBy (ground, distanceVector, seconds);
+			break;
+		}
+
+		StartCoroutine (MoveBack ());
+	}
+
+	IEnumerator MoveBack ()
+	{
+		yield return new WaitForSeconds (seconds);
+
+		switch (direction) {
+		case Direction.left:
+			iTween.MoveBy (ground, distanceVector, 0.5f);
+			break;
+		case Direction.right:
+			iTween.MoveBy (ground, -distanceVector, 0.5f);
+			break;
+		}
+
+		active = false;
+		GetComponent<SpriteRenderer> ().sprite = inactiveSprite;
+	}
+}
